d937e842fb7fc64c80f71592b050a9f4938b0ee8
Shimaz <shimaz.bsh@gmail.com>
2014-11-24 00:51:44 +0900

    공유 시작

---
+++ AlumList/src/com/bshlab/alumlist/BookmarkActivity.java
@@ -0,0 +1,7 @@
+package com.bshlab.alumlist;
+
+import android.app.Activity;
+
+public class BookmarkActivity extends Activity {
+
+}
---
+++ AlumList/src/com/bshlab/alumlist/PublicData.java
@@ -0,0 +1,7 @@
+package com.bshlab.alumlist;
+
+import android.app.Application;
+
+public class PublicData extends Application {
+
+}
