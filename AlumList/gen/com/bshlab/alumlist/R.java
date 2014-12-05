468d840840cd8b09615f04577799eeaab089f6d8
Shimaz <shimaz.bsh@gmail.com>
2014-12-05 22:28:43 +0900

    add animation xml

--- AlumList/gen/com/bshlab/alumlist/R.java
+++ AlumList/gen/com/bshlab/alumlist/R.java
@@ -8,6 +8,11 @@
 package com.bshlab.alumlist;
 
 public final class R {
+    public static final class anim {
+        public static final int fade_in_out=0x7f040000;
+        public static final int fade_in_short=0x7f040001;
+        public static final int fade_out_short=0x7f040002;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -18,8 +23,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -29,9 +34,9 @@ public final class R {
         public static final int layout_start=0x7f030001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -55,10 +60,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
