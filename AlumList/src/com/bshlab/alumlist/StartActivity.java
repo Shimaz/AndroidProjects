ea98d538b68d3fbd5ab8772c7799531c3ba1a384
Shimaz <shimaz.bsh@gmail.com>
2015-01-24 20:04:27 +0900

    123

--- AlumList/src/com/bshlab/alumlist/StartActivity.java
+++ AlumList/src/com/bshlab/alumlist/StartActivity.java
@@ -70,7 +70,7 @@ public class StartActivity extends Activity {
 		
 		
 	}
-//
+//	
 //	@Override
 //	public boolean onCreateOptionsMenu(Menu menu) {
 //		// Inflate the menu; this adds items to the action bar if it is present.
