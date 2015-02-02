d26b6a5870fe70e4fb877d7cb971cc2b2920988c
Shimaz <shimaz.bsh@gmail.com>
2015-02-03 02:08:56 +0900

    pad_g started

---
+++ Leeum.yhg.pad_g/Leeum.yhg.pad_g/gen/kr/tangomike/leeum/yhg/pad_g/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package kr.tangomike.leeum.yhg.pad_g;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Leeum.yhg.pad_g/Leeum.yhg.pad_g/src/kr/tangomike/leeum/yhg/pad_g/StartActivity.java
@@ -0,0 +1,41 @@
+package kr.tangomike.leeum.yhg.pad_g;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.RelativeLayout;
+
+public class StartActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle sis){
+		super.onCreate(sis);
+		
+		RelativeLayout rl = new RelativeLayout(this);
+		rl.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		
+		setContentView(rl);
+		
+		rl.setBackgroundResource(R.drawable.b_cover);
+		
+		rl.setOnTouchListener(new OnTouchListener(){
+
+			@Override
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				// TODO Auto-generated method stub
+				
+				Intent intent = new Intent(StartActivity.this, MainActivity.class);
+				startActivity(intent);
+				overridePendingTransition(R.anim.fade_in_short, R.anim.fade_out_short);
+				return false;
+			}
+			
+		});
+		
+	}
+	
+
+}
