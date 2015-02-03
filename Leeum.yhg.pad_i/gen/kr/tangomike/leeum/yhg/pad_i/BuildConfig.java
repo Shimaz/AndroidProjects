e48abab36ada622b550a3b65bd83f08ae093be28
Shimaz <shimaz.bsh@gmail.com>
2015-02-04 06:06:23 +0900

    pad i added

---
+++ Leeum.yhg.pad_i/gen/kr/tangomike/leeum/yhg/pad_i/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package kr.tangomike.leeum.yhg.pad_i;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Leeum.yhg.pad_i/src/kr/tangomike/leeum/yhg/pad_i/StartActivity.java
@@ -0,0 +1,45 @@
+package kr.tangomike.leeum.yhg.pad_i;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.RelativeLayout;
+
+public class StartActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle sis){
+		super.onCreate(sis);
+		
+		RelativeLayout rl = new RelativeLayout(this);
+		rl.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+		
+		setContentView(rl);
+		
+		/*Disable Sleep Mode */
+        super.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		
+		rl.setBackgroundResource(R.drawable.i_cover);
+		
+		rl.setOnTouchListener(new OnTouchListener(){
+
+			@Override
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				// TODO Auto-generated method stub
+				
+				Intent intent = new Intent(StartActivity.this, MainActivity.class);
+				startActivity(intent);
+				overridePendingTransition(R.anim.fade_in_short, R.anim.fade_out_short);
+				return false;
+			}
+			
+		});
+		
+	}
+	
+
+}
