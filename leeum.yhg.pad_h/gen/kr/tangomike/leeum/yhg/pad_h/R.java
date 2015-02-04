e42604a909e8afcfd2fe03c7b22f2646ba80d9c2
Shimaz <shimaz.bsh@gmail.com>
2015-02-04 19:57:19 +0900

    pad h started

--- leeum.yhg.pad_h/gen/kr/tangomike/leeum/yhg/pad_h/R.java
+++ leeum.yhg.pad_h/gen/kr/tangomike/leeum/yhg/pad_h/R.java
@@ -11,13 +11,30 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int h_cover=0x7f020000;
+        public static final int h_text_eng_1=0x7f020001;
+        public static final int h_text_eng_2=0x7f020002;
+        public static final int h_text_eng_3=0x7f020003;
+        public static final int h_text_eng_4=0x7f020004;
+        public static final int h_text_eng_5=0x7f020005;
+        public static final int h_text_eng_6=0x7f020006;
+        public static final int h_text_kor_1=0x7f020007;
+        public static final int h_text_kor_2=0x7f020008;
+        public static final int h_text_kor_3=0x7f020009;
+        public static final int h_text_kor_4=0x7f02000a;
+        public static final int h_text_kor_5=0x7f02000b;
+        public static final int h_title_eng=0x7f02000c;
+        public static final int h_title_kor=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
+    public static final class raw {
+        public static final int lov_mov=0x7f040000;
+    }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
     }
 }
