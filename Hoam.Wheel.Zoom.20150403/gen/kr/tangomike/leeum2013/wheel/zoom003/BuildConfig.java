e5e71d069f66202267174712d9f494c5b8757a53
Shimaz <shimaz.bsh@gmail.com>
2015-03-31 17:55:15 +0900

    Hoam Helmet with Settings Button

--- Hoam.Wheel.Zoom.20150403/gen/kr/tangomike/leeum2013/wheel/zoom003/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package kr.tangomike.leeum2013.wheel.zoom003;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
